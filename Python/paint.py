da8c81d3a9a8c720107a3c89dd75ae23bb2d81bb
A01027251 <a01027251@itesm.mx>
2021-05-06 10:07:36 -0500

    Rectangulo completo

--- Python/paint.py
+++ Python/paint.py
@@ -38,8 +38,19 @@ def circle(start, end):
     pass  # TODO
 
 def rectangle(start, end):
-    "Draw rectangle from start to end."
-    pass  # TODO
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+
+    for count in range(2):
+        forward(end.x - start.x)
+        left(90)
+        forward((end.x - start.y)-end.x)
+        left(90)
+
+    end_fill()
+
 
 def triangle(start, end):
     "Draw triangle from start to end."
